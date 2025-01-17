c124e089eb67295572d3927bcec142045749afeb
SandhyaReddy <158995969+SandhyaReddy-2713@users.noreply.github.com>
2025-01-17 16:27:31 +0530

    Update playlist.tf

--- playlist.tf
+++ playlist.tf
@@ -1,14 +1,17 @@
-resource "spotify_playlist" "favourite" {
-  name = "favourites"
-  tracks = ["4ZDbBaMwzedRPdqpH7qsWX"]
+# TO CREATE A SPOTIFY PLAYLIST USING TRACKS:
+resource "spotify_playlist" "<PLAYLIST_NAME>" {
+  name = "<PLAY_LIST_NAME>"
+  tracks = ["PASTE_MUSIC_TRACK_ID"]
 }
 
-data "spotify_search_track" "karthik" {
-  artist = "karthik"
+
+# TO CREATE SPOTIFY PLAYLIST USING DATA BLOCK:
+data "spotify_search_track" "<ARTIST_NAME>" {
+  artist = "<ARTIST_NAME>"
   # limit = 5
 }
-resource "spotify_playlist" "karthik_music" {
-    name = "karthik"
-    tracks = [data.spotify_search_track.karthik.tracks[7].id,
-    data.spotify_search_track.karthik.tracks[8].id] 
+resource "spotify_playlist" "<PLAY_LIST_NAME>" {
+    name = "<PLAY_LIST_NAME"
+    tracks = [data.spotify_search_track.<ARTIST_NAME>.tracks[0].id,
+    data.spotify_search_track.<ARTIST_NAME>.tracks[1].id] 
 }
